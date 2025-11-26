26b34684b292f1c12a49bfe2d2fd924efa30be64
Steffan Andrews <stef@orchetect.com>
2025-11-25 17:16:02 -0800

    Removed static library requirement

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
     products: [
         .library(
             name: "swift-extensions",
-            type: .static,
             targets: ["SwiftExtensions"]
         )
     ],
