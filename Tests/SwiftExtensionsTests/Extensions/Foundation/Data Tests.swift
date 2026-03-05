e1c8af8b0923a87e61870c1b96f04395ac06b8d7
Steffan Andrews <stef@orchetect.com>
2026-03-04 22:28:52 -0800

    Updated unit tests

--- Tests/SwiftExtensionsTests/Extensions/Foundation/Data Tests.swift
+++ Tests/SwiftExtensionsTests/Extensions/Foundation/Data Tests.swift
@@ -297,6 +297,95 @@ import TestingExtensions
         )
     }
     
+    @Test
+    func int32_data_pointer() {
+        // .toInt32
+        
+        let data = Data([1, 2, 3, 4])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toInt32(from: .littleEndian)
+                    == 0b00000100_00000011_00000010_00000001
+            )
+            #expect(
+                buffer.toInt32(from: .bigEndian)
+                    == 0b00000001_00000010_00000011_00000100
+            )
+        }
+    }
+    
+    @Test
+    func int32_data_subsequence_pointer() {
+        // .toInt32
+        
+        let baseData = Data([99, 1, 2, 3, 4])
+        let data = baseData[1 ... 4]
+        
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toInt32(from: .littleEndian)
+                    == 0b00000100_00000011_00000010_00000001
+            )
+            #expect(
+                buffer.toInt32(from: .bigEndian)
+                    == 0b00000001_00000010_00000011_00000100
+            )
+        }
+    }
+    
+    @Test
+    func int32_rawPointer() {
+        // .toInt32
+        
+        let data = Data([1, 2, 3, 4])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            #expect(
+                buffer.toInt32(from: .littleEndian)
+                    == 0b00000100_00000011_00000010_00000001
+            )
+            #expect(
+                buffer.toInt32(from: .bigEndian)
+                    == 0b00000001_00000010_00000011_00000100
+            )
+        }
+    }
+    
+    @Test
+    func int32_rawPointer_slice() {
+        // .toInt32
+        
+        let data = Data([99, 1, 2, 3, 4])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            let subdata: Slice<UnsafeRawBufferPointer> = buffer[1 ... 4]
+            #expect(
+                subdata.toInt32(from: .littleEndian)
+                    == 0b00000100_00000011_00000010_00000001
+            )
+            #expect(
+                subdata.toInt32(from: .bigEndian)
+                    == 0b00000001_00000010_00000011_00000100
+            )
+        }
+    }
+    
+    @Test
+    func int32_uInt8Pointer_slice() {
+        // .toInt32
+        
+        let data = Data([99, 1, 2, 3, 4])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            let subdata: Slice<UnsafeBufferPointer<UInt8>> = buffer[1 ... 4]
+            #expect(
+                subdata.toInt32(from: .littleEndian)
+                    == 0b00000100_00000011_00000010_00000001
+            )
+            #expect(
+                subdata.toInt32(from: .bigEndian)
+                    == 0b00000001_00000010_00000011_00000100
+            )
+        }
+    }
+    
     #if !(arch(arm) || arch(arm64_32) || arch(i386))
     @Test
     func int64() {
@@ -749,6 +838,85 @@ import TestingExtensions
     }
     
     @Test
+    func float32_data_pointer() {
+        // .toFloat32
+        
+        let data = Data([1, 2, 3, 4])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toFloat32(from: .littleEndian) == 1.5399896e-36
+            )
+            #expect(
+                buffer.toFloat32(from: .bigEndian) == 2.3879393e-38
+            )
+        }
+    }
+    
+    @Test
+    func float32_data_subsequence_pointer() {
+        // .toFloat32
+        
+        let baseData = Data([99, 1, 2, 3, 4])
+        let data = baseData[1 ... 4]
+        
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toFloat32(from: .littleEndian) == 1.5399896e-36
+            )
+            #expect(
+                buffer.toFloat32(from: .bigEndian) == 2.3879393e-38
+            )
+        }
+    }
+    
+    @Test
+    func float32_rawPointer() {
+        // .toFloat32
+        
+        let data = Data([1, 2, 3, 4])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            #expect(
+                buffer.toFloat32(from: .littleEndian) == 1.5399896e-36
+            )
+            #expect(
+                buffer.toFloat32(from: .bigEndian) == 2.3879393e-38
+            )
+        }
+    }
+    
+    @Test
+    func float32_rawPointer_slice() {
+        // .toFloat32
+        
+        let data = Data([99, 1, 2, 3, 4])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            let subdata: Slice<UnsafeRawBufferPointer> = buffer[1 ... 4]
+            #expect(
+                subdata.toFloat32(from: .littleEndian) == 1.5399896e-36
+            )
+            #expect(
+                subdata.toFloat32(from: .bigEndian) == 2.3879393e-38
+            )
+        }
+    }
+    
+    @Test
+    func float32_uInt8Pointer_slice() {
+        // .toFloat32
+        
+        let data = Data([99, 1, 2, 3, 4])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            let subdata: Slice<UnsafeBufferPointer<UInt8>> = buffer[1 ... 4]
+            #expect(
+                subdata.toFloat32(from: .littleEndian) == 1.5399896e-36
+            )
+            #expect(
+                subdata.toFloat32(from: .bigEndian) == 2.3879393e-38
+            )
+        }
+    }
+    
+    @Test
     func double() {
         // .toData
         
@@ -819,6 +987,85 @@ import TestingExtensions
             ([1, 2, 3, 4, 5, 6, 7, 8] as [UInt8]).toDouble(from: .bigEndian)
                 == 8.20788039913184e-304
         )
+    }
+    
+    @Test
+    func double_data_pointer() {
+        // .toDouble
+        
+        let data = Data([1, 2, 3, 4, 5, 6, 7, 8])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toDouble(from: .littleEndian) == 5.447603722011605e-270
+            )
+            #expect(
+                buffer.toDouble(from: .bigEndian) == 8.20788039913184e-304
+            )
+        }
+    }
+    
+    @Test
+    func double_data_subsequence_pointer() {
+        // .toDouble
+        
+        let baseData = Data([99, 1, 2, 3, 4, 5, 6, 7, 8])
+        let data = baseData[1 ... 8]
+        
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            #expect(
+                buffer.toDouble(from: .littleEndian) == 5.447603722011605e-270
+            )
+            #expect(
+                buffer.toDouble(from: .bigEndian) == 8.20788039913184e-304
+            )
+        }
+    }
+    
+    @Test
+    func double_rawPointer() {
+        // .toDouble
+        
+        let data = Data([1, 2, 3, 4, 5, 6, 7, 8])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            #expect(
+                buffer.toDouble(from: .littleEndian) == 5.447603722011605e-270
+            )
+            #expect(
+                buffer.toDouble(from: .bigEndian) == 8.20788039913184e-304
+            )
+        }
+    }
+    
+    @Test
+    func double_rawPointer_slice() {
+        // .toDouble
+        
+        let data = Data([99, 1, 2, 3, 4, 5, 6, 7, 8])
+        data.withUnsafeBytes { buffer in // UnsafeRawBufferPointer
+            let subdata: Slice<UnsafeRawBufferPointer> = buffer[1 ... 8]
+            #expect(
+                subdata.toDouble(from: .littleEndian) == 5.447603722011605e-270
+            )
+            #expect(
+                subdata.toDouble(from: .bigEndian) == 8.20788039913184e-304
+            )
+        }
+    }
+    
+    @Test
+    func double_uInt8Pointer_slice() {
+        // .toDouble
+        
+        let data = Data([99, 1, 2, 3, 4, 5, 6, 7, 8])
+        data.withContiguousStorageIfAvailable { buffer in // UnsafeBufferPointer<UInt8>
+            let subdata: Slice<UnsafeBufferPointer<UInt8>> = buffer[1 ... 8]
+            #expect(
+                subdata.toDouble(from: .littleEndian) == 5.447603722011605e-270
+            )
+            #expect(
+                subdata.toDouble(from: .bigEndian) == 8.20788039913184e-304
+            )
+        }
     }
     
     @Test
