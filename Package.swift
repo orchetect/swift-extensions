678ab7076adf6b92aad165de0fc494e8132095f8
Steffan Andrews <stef@orchetect.com>
2026-03-18 23:01:45 -0700

    Bumped swift-data-parsing dependency to 0.1.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // External dependencies
-        .package(url: "https://github.com/orchetect/swift-data-parsing", from: "0.1.0"),
+        .package(url: "https://github.com/orchetect/swift-data-parsing", from: "0.1.1"),
         
         // Testing-only dependencies
         .package(url: "https://github.com/apple/swift-numerics", from: "1.1.0"),
