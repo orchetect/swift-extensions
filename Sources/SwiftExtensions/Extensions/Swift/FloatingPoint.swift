b9e250de2867303248d4ea293f808be1868e6db1
Steffan Andrews <stef@orchetect.com>
2026-04-04 15:56:08 -0700

    Code organization

--- Sources/SwiftExtensions/Extensions/Swift/FloatingPoint.swift
+++ Sources/SwiftExtensions/Extensions/Swift/FloatingPoint.swift
@@ -155,7 +155,7 @@ extension BinaryFloatingPoint {
     public var boolValue: Bool { self > 0.0 }
 }
 
-// MARK: - .truncated() / .rounded
+// MARK: - Rounded
 
 extension FloatingPoint where Self: FloatingPointPowerComputable {
     /// Rounds to `decimalPlaces` number of decimal places using rounding `rule`.
@@ -185,6 +185,71 @@ extension FloatingPoint where Self: FloatingPointPowerComputable {
         decimalPlaces: Int
     ) {
         self = rounded(rule, decimalPlaces: decimalPlaces)
+    }
+}
+
+// MARK: - Truncated
+
+extension FloatingPoint where Self: FloatingPointPowerComputable {
+    /// Replaces this value by truncating it to `decimalPlaces` number of decimal places.
+    ///
+    /// If `decimalPlaces <= 0`, then `self.rounded(.towardZero)` is returned.
+    @_disfavoredOverload
+    public mutating func truncate(decimalPlaces: Int) {
+        self = truncated(decimalPlaces: decimalPlaces)
+    }
+    
+    /// Truncates decimal places to `decimalPlaces` number of decimal places.
+    ///
+    /// If `decimalPlaces <= 0`, then `self.rounded(.towardZero)` is returned.
+    @_disfavoredOverload
+    public func truncated(decimalPlaces: Int) -> Self {
+        if decimalPlaces < 1 {
+            return rounded(.towardZero)
+        }
+        
+        let offset = Self(10).power(Self(decimalPlaces))
+        return (self * offset).rounded(.towardZero) / offset
+    }
+}
+
+// MARK: - Integral and Fraction
+
+extension FloatingPoint {
+    /// Similar to `Int.quotientAndRemainder(dividingBy:)` from the Swift standard library.
+    @_disfavoredOverload
+    public func quotientAndRemainder(dividingBy rhs: Self) -> (quotient: Self, remainder: Self) {
+        let calculation = self / rhs
+        let integral = calculation.rounded(.towardZero)
+        let fraction = self - (integral * rhs)
+        return (quotient: integral, remainder: fraction)
+    }
+    
+    /// Returns both integral part and fractional part.
+    ///
+    /// - Note: This method is more computationally efficient than calling both `integral` and
+    ///   `fraction` properties separately unless you only require one or the other.
+    ///
+    /// This method can result in a non-trivial loss of precision for the fractional part.
+    @inlinable @_disfavoredOverload
+    public var integralAndFraction: (integral: Self, fraction: Self) {
+        let integral = rounded(.towardZero)
+        let fraction = self - integral
+        return (integral: integral, fraction: fraction)
+    }
+    
+    /// Returns the integral part (digits before the decimal point).
+    @inlinable @_disfavoredOverload
+    public var integral: Self {
+        integralAndFraction.integral
+    }
+    
+    /// Returns the fractional part (digits after the decimal point).
+    ///
+    /// - Note: this method can result in a non-trivial loss of precision for the fractional part.
+    @inlinable @_disfavoredOverload
+    public var fraction: Self {
+        integralAndFraction.fraction
     }
 }
 
--- Tests/SwiftExtensionsTests/Extensions/Darwin/FloatingPoint and Darwin Tests.swift
+++ Tests/SwiftExtensionsTests/Extensions/Darwin/FloatingPoint and Darwin Tests.swift
@@ -34,63 +34,6 @@ import Testing
         #expect(Float80(2.0).power(3) == 8.0)
         #endif
     }
-    
-    @Test
-    func truncated() async {
-        // Double .truncated()
-        
-        #expect(1.1234.truncated(decimalPlaces: -1) == 1.0)
-        #expect(1.1234.truncated(decimalPlaces: 0) ==  1.0)
-        #expect(1.1234.truncated(decimalPlaces: 2) ==  1.12)
-        #expect(1.1234.truncated(decimalPlaces: 3) ==  1.123)
-        #expect(1.1234.truncated(decimalPlaces: 4) ==  1.1234)
-        #expect(1.1234.truncated(decimalPlaces: 5) ==  1.1234)
-        
-        #expect(0.123456789.truncated(decimalPlaces: 8) == 0.12345678)
-        
-        var dbl = 0.1264
-        dbl.truncate(decimalPlaces: 2)
-        #expect(dbl == 0.12)
-        
-        // Float .truncated()
-        
-        var flt: Float = 1.1234
-        
-        #expect(flt.truncated(decimalPlaces: -1) == 1.0)
-        #expect(flt.truncated(decimalPlaces: 0) ==  1.0)
-        #expect(flt.truncated(decimalPlaces: 2) ==  1.12)
-        #expect(flt.truncated(decimalPlaces: 3) ==  1.123)
-        #expect(flt.truncated(decimalPlaces: 4) ==  1.1234)
-        #expect(flt.truncated(decimalPlaces: 5) ==  1.1234)
-        
-        // flt = 0.123456789
-        // #expect(flt.truncated(decimalPlaces: 8) == 0.12345678) // fails -- precision issue??
-        
-        // Double .truncate()
-        
-        flt = 0.1264
-        flt.truncate(decimalPlaces: 2)
-        #expect(flt == 0.12)
-    }
-    
-    @Test
-    func quotientAndRemainder() async {
-        let qr = 17.5.quotientAndRemainder(dividingBy: 5.0)
-        
-        #expect(qr.quotient == 3)
-        #expect(qr.remainder == 2.5)
-    }
-    
-    @Test
-    func integralAndFraction() async {
-        let iaf = 17.5.integralAndFraction
-        
-        #expect(iaf.integral == 17)
-        #expect(iaf.fraction == 0.5)
-        
-        #expect(17.5.integral == 17)
-        #expect(17.5.fraction == 0.5)
-    }
 }
 
 #endif
