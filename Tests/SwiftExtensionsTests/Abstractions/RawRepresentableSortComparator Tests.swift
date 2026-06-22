5e397838747f67eb3e298424b903e228c0c7b0a2
Steffan Andrews <stef@orchetect.com>
2026-06-22 16:14:46 -0700

    Updated unit tests

--- Tests/SwiftExtensionsTests/Abstractions/RawRepresentableSortComparator Tests.swift
+++ Tests/SwiftExtensionsTests/Abstractions/RawRepresentableSortComparator Tests.swift
@@ -20,9 +20,53 @@ import Testing
         }
         let array: [Foo] = [.three, .one, .two]
 
-        // comparator used directly
-        let sortedForward = array.sorted(using: RawRepresentableSortComparator())
-        #expect(sortedForward == [.one, .two, .three])
+        // using type inference
+        do {
+            let sortedForward = array.sorted(using: RawRepresentableSortComparator())
+            #expect(sortedForward == [.one, .two, .three])
+
+            let sortedReverse = array.sorted(using: RawRepresentableSortComparator(order: .reverse))
+            #expect(sortedReverse == [.three, .two, .one])
+        }
+
+        // supplying type explicitly
+        do {
+            let sortedForward = array.sorted(using: RawRepresentableSortComparator(type: Foo.self))
+            #expect(sortedForward == [.one, .two, .three])
+        }
+
+        do {
+            let sortedReverse = array.sorted(using: RawRepresentableSortComparator(type: Foo.self, order: .reverse))
+            #expect(sortedReverse == [.three, .two, .one])
+        }
+    }
+
+    @Test
+    func sortedUsing_wrapping() {
+        enum Foo: String {
+            case one = "a"
+            case two = "A"
+            case three = "b"
+        }
+        let array: [Foo] = [.three, .two, .one]
+
+        // using type inference
+        do {
+            let sortedForward = array.sorted(using: RawRepresentableSortComparator(wrapping: .localized))
+            #expect(sortedForward == [.one, .two, .three])
+
+            let sortedReverse = array.sorted(using: RawRepresentableSortComparator(wrapping: .localized, order: .reverse))
+            #expect(sortedReverse == [.one, .two, .three])
+        }
+
+        // supplying type explicitly
+        do {
+            let sortedForward = array.sorted(using: RawRepresentableSortComparator(type: Foo.self, wrapping: .localized))
+            #expect(sortedForward == [.one, .two, .three])
+
+            let sortedReverse = array.sorted(using: RawRepresentableSortComparator(type: Foo.self, wrapping: .localized, order: .reverse))
+            #expect(sortedReverse == [.one, .two, .three])
+        }
     }
 }
 
